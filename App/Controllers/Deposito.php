e7f418a382a1be8a965ade826c51980a8ca5fe6d
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-05 11:09:34 +0700

    migrate modeling from controller to model

--- App/Controllers/Deposito.php
+++ App/Controllers/Deposito.php
@@ -1,27 +1,24 @@
 <?php
 included('BaseController', 'c');
+included('DepositoModel', 'm');
 
 class Deposito extends BaseController
 {
+  private $DepositoModel;
 
   public function __construct()
   {
+    $this->DepositoModel = new DepositoModel;
     parent::__construct();
   }
 
   public function index()
   {
-    $historyCalculation = Database::connect()->query("SELECT * FROM `history_calculation`");
-
-    $hc = array();
-
-    while ($row = mysqli_fetch_assoc($historyCalculation)) {
-      $hc[] = $row;
-    }
+    $historyCalculation = $this->DepositoModel->getCalculationHistory();
 
     return view('deposito.index', [
-      'title' => 'Deposito Calculation',
-      'historyCalculation' => $hc,
+      'title'              => 'Deposito Calculation',
+      'historyCalculation' => $historyCalculation,
     ]);
   }
 
@@ -32,10 +29,9 @@ class Deposito extends BaseController
     $timeSpan     = $this->request->getVar('time_span');
 
     if (is_numeric($amountMoney) && is_numeric($interestRate) && is_numeric($timeSpan)) {
-
       $result = ($amountMoney * ($interestRate / 100) * $timeSpan);
 
-      Database::connect()->query("INSERT INTO `history_calculation` VALUES (null, $amountMoney, $interestRate, $timeSpan)");
+      $this->DepositoModel->saveCalculationHistory($amountMoney, $interestRate, $timeSpan);
 
       return redirect("deposito/?amountMoney=$amountMoney&interestRate=$interestRate&timeSpan=$timeSpan&result=$result");
     } else {
--- App/Models/DepositoModel.php
+++ App/Models/DepositoModel.php
@@ -3,4 +3,22 @@ require_once SYSTEMPATH . 'Database.php';
 
 class DepositoModel extends Database
 {
+  private $table = 'history_calculation';
+
+  public function saveCalculationHistory($amountMoney, $interestRate, $timeSpan)
+  {
+    return $this->connect()->query("INSERT INTO `{$this->table}` VALUES (null, $amountMoney, $interestRate, $timeSpan)");
+  }
+
+  public function getCalculationHistory()
+  {
+    $result = $this->connect()->query("SELECT * FROM `{$this->table}`");
+    $arrayAssoc = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+      $arrayAssoc[] = $row;
+    }
+
+    return $arrayAssoc;
+  }
 }
