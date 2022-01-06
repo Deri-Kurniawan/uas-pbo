2d3a22aa70fb2373427718734f7e4a6d270368fc
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-06 15:34:44 +0700

    change navbar brand

--- App/Views/layouts/components/navbar.php
+++ App/Views/layouts/components/navbar.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 shadow rounded">
   <div class="container">
-    <a class="navbar-brand" href="<?= base_url(); ?>">DepoCalc</a>
+    <a class="navbar-brand" href="<?= base_url(); ?>"><img src="<?= public_path('favicon.png') ?>" width="25em"
+        style="margin-right: .1em">epo Calculator</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
