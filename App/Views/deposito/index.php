a05d3649682fdfc4cfac6db815d02083ecad8565
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-06 13:39:27 +0700

    add clear history btn, condition & fix el layout

--- App/Views/deposito/index.php
+++ App/Views/deposito/index.php
@@ -5,15 +5,28 @@
   <div class="container">
     <div class="col-12 col-md-8 offset-md-2">
       <h1 class="text-center">Kalkulator Deposito</h1>
+
+      <?php if (isset($_GET['successMessage'])) : ?>
+      <div class="alert alert-success alert-dismissible fade show text-center">
+        <?= $_GET['successMessage']; ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php endif ?>
+
       <?php if (isset($_GET['errorMessage'])) : ?>
-      <div class="alert alert-danger text-center"><?= $_GET['errorMessage']; ?></div>
+      <div class="alert alert-danger alert-dismissible fade show text-center">
+        <?= $_GET['errorMessage']; ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
       <?php endif ?>
 
       <?php if (isset($_GET['result'])) : ?>
-      <div class="my-4 text-center">
+      <div class="alert alert-success alert-dismissible fade show text-center">
         Rp<?= number_format($_GET['amountMoney'], 2) ?> ×
         <?= $_GET['interestRate']; ?>% ×
-        <?= $_GET['timeSpan']; ?> Tahun = Rp<?= number_format($_GET['result'], 2); ?>
+        <?= $_GET['timeSpan']; ?> Tahun = <span
+          class="text-decoration-underline">Rp<?= number_format($_GET['result'], 2); ?></span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
       <?php endif ?>
 
@@ -21,14 +34,14 @@
         <div class="mt-2">
           <label for="amount_money">Jumlah Uang</label>
           <input type="number" name="amount_money" id="amount_money" class="form-control"
-            value="<?= isset($_GET['amv']) ? $_GET['amv'] : ''; ?>" placeholder="Jumlah Uang" required>
+            value="<?= isset($_GET['amv']) ? $_GET['amv'] : ''; ?>" placeholder="Masukan angka" required>
         </div>
         <div class="row">
           <div class="col-12 mt-2 col-sm-6">
             <label for="interest_rate">Suku Bunga</label>
             <div class="input-group">
               <input type="interest_rate" name="interest_rate" id="interest_rate" class="form-control"
-                value="<?= isset($_GET['irv']) ? $_GET['irv'] : ''; ?>" placeholder="Suku Bunga" required>
+                value="<?= isset($_GET['irv']) ? $_GET['irv'] : ''; ?>" placeholder="Masukan angka" required>
               <span class="input-group-text">%</span>
             </div>
           </div>
@@ -36,7 +49,7 @@
             <label for="time_span">Jangka Waktu</label>
             <div class="input-group">
               <input type="time_span" name="time_span" id="time_span" class="form-control"
-                value="<?= isset($_GET['tsv']) ? $_GET['tsv'] : ''; ?>" placeholder="Jangka Waktu" required>
+                value="<?= isset($_GET['tsv']) ? $_GET['tsv'] : ''; ?>" placeholder="Masukan angka" required>
               <span class="input-group-text">Tahun</span>
             </div>
           </div>
@@ -49,7 +62,12 @@
       <hr>
 
       <h2 class="text-center my-4">Sejarah Kalkulasi</h2>
-      <div class="col-12" style="overflow: scroll;">
+      <?php if (is_array($historyCalculation) && count($historyCalculation) > 0) : ?>
+      <a class="btn btn-outline-danger mb-3" href="<?= base_url(); ?>Deposito/clearHistory"
+        onclick="return confirm('Data tidak akan pernah dapat dikembalikan.\nyakin ingin membersihkan?')">Bersihkan
+        Sejarah</a>
+      <?php endif ?>
+      <div class="col-12" id="box-scroll">
         <table class="table table-hover table-bordered">
           <tr class="table-primary">
             <th>No.</th>
