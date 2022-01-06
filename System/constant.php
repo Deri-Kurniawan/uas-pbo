3430086e3a0e44df43531a4fbe2f4e7ecd34846f
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-06 13:34:00 +0700

    add public path constant

--- System/constant.php
+++ System/constant.php
@@ -8,5 +8,9 @@ define("APPPATH", 'App/');
  * System Path
  */
 define("SYSTEMPATH", 'System/');
+/**
+ * Public Path
+ */
+define("PUBLICPATH", 'Public/');
 
 require_once APPPATH . 'Configs/App.php';
